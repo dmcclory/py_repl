47805779fe8d47f1e8c179223622feb53f452aac
Dan McClory <danmcclory@gmail.com>
2014-04-03 12:13:17 -0400

    wrap Redis eval in a class, so we don't clobber Kernel#eval

--- redis_repl.rb
+++ redis_repl.rb
@@ -1,30 +1,40 @@
 require 'socket'
 require 'readline'
 
-@s = TCPSocket.new 'localhost', 6379
+class RedisInterface
+  def initialize
+    @socket = TCPSocket.new 'localhost', 6379
+  end
 
-def to_resp(command)
-  terms = command.split(" ")
-  length_marker = "*#{terms.length}\r\n"
-  length_marker + terms.map { |term| ["$#{term.length}", term] }.flatten.join("\r\n") + "\r\n"
+  def to_resp(command)
+    terms = command.split(" ")
+    length_marker = "*#{terms.length}\r\n"
+    length_marker + terms.map { |term| ["$#{term.length}", term] }.flatten.join("\r\n") + "\r\n"
+  end
+
+  def eval(command)
+    @socket.write to_resp(command)
+    IO.select([@socket])
+    # still need to convert these results from RASP
+    # need to figure out a way to read bigger chunks:
+    # not sure why, but read_nonblock works WAAAY better for this!
+    #   - read blocks ... until the connection closes?
+    @socket.read_nonblock(500)
+  end
+
+  def close
+    @socket.close
+  end
 end
 
-def eval(command)
-  @s.write to_resp(command)
-  IO.select([@s])
-  # still need to convert these results from RASP
-  # need to figure out a way to read bigger chunks:
-  # not sure why, but read_nonblock works WAAAY better for this!
-  #   - read blocks ... until the connection closes?
-  @s.read_nonblock(500)
-end
+redis = RedisInterface.new
 
 while buf = Readline.readline("> ", true)
   if buf == "exit"
     break
   else
-    p eval(buf)
+    p redis.eval(buf)
   end
 end
 
-@s.close
+redis.close
