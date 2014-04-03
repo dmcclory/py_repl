cc6b19cb77dfab529245c4e48933148bf6519955
Dan McClory <danmcclory@gmail.com>
2014-04-03 08:23:55 -0400

    commit a failing test ... watch out

--- test/can_get_values_from_python_test.rb
+++ test/can_get_values_from_python_test.rb
@@ -12,7 +12,7 @@ class PyClySimpleExpressions < Minitest::Test
 
   def test_that_py_cly_can_return_information_about_parse_errors
     result = @py_cly.eval('print 2 + ').chomp
-    assert_equal 'invalid syntax', result
+    assert_match 'invalid syntax', result
   end
 
   def test_that_py_cly_can_eval_multiple_expressions_in_same_space
@@ -26,5 +26,13 @@ class PyClySimpleExpressions < Minitest::Test
     assert_equal "", statement_output
     variable_lookup = @py_cly.eval('a').chomp
     assert_equal "2", variable_lookup
+  end
+
+  def test_that_py_cly_can_define_functions
+    statement_output = @py_cly.eval(FIXTURES[:square_function]).chomp
+    require 'pry'; binding.pry
+    result = @py_cly.eval('square(10)').chomp
+    assert_equal "100", result
+
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,9 @@ require 'minitest/autorun'
 
 $LOAD_PATH.unshift "lib"
 require 'py_cly'
+
+FIXTURES = {
+
+  :square_function => "def square(x):\n  return x * x\n\n"
+
+}
