e4e4e3c36b7c3a9e0042c039af699776db5556a6
Dan McClory <danmcclory@gmail.com>
2014-04-05 13:20:52 -0400

    make py_repl more pythonic
    introduces a secondary input loop, in rescue ParseError block. this will
    read all the until the next blank line, and evaluate all of that text at
    once. just like python's repl. fixes a bug where the REPL would evaluate
    the smallest parseable unit of code.
    
    uses '>>>' and '...' for primary and seconday input modes

--- py_repl.rb
+++ py_repl.rb
@@ -3,7 +3,7 @@ require 'py_cly'
 
 py_cly = PyCly.new
 
-while buf = Readline.readline("> ", true)
+while buf = Readline.readline(">>>", true)
   buf += "\n\n"
   if buf == "exit\n\n"
     break
@@ -13,8 +13,12 @@ while buf = Readline.readline("> ", true)
       buf = ""
     rescue PyCly::ParseError => e
       buf.chomp! if buf.match(/\n\n$/)
-      buf += Readline.readline("..", true)
-      buf += "\n\n"
+      while subbuf = Readline.readline("...", true)
+        break if subbuf == ""
+        buf += subbuf
+        buf += "\n"
+      end
+      buf += "\n"
       retry
     end
   end
